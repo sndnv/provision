bed1665e8f6c67c102178cb229801f41a4affc84
Angel Sanadinov <sndnv@users.noreply.github.com>
2024-02-13 10:16:44 +0100

    Fix python and ansible deprecations

--- laptop/pull.sh
+++ laptop/pull.sh
@@ -3,7 +3,7 @@
 echo ">: Installing git and ansible..."
 sudo apt update
 sudo apt install git python3 python3-pip python3-setuptools sshpass -y
-sudo pip3 install ansible
+sudo pip3 install ansible --break-system-packages
 
 echo ">: Pulling ansible provisioning/bootstrap..."
 bootstrap_directory="/tmp/bootstrap"
